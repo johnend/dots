18cb969d6922b573e45f1b3adbc892245c490dc1
John Enderby <john.enderby.1987@gmail.com>
2024-05-27 22:44:28 +0100

    nvim: precognition issues on linuz
    this might need some investigation as it worked on mac

--- .config/nvim/lua/core/plugin_config/precognition.lua
+++ .config/nvim/lua/core/plugin_config/precognition.lua
@@ -1,7 +1,9 @@
 local precognition = require "precognition"
 
 precognition.setup {
-  highlightColor = "Comment",
+  highlightColor = {
+    link = "Comment",
+  },
 }
 
 vim.keymap.set("n", "<leader>tp", ":lua require('precognition').toggle()<CR>", { desc = "Toggle precognition" })
