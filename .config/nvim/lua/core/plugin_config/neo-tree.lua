d2571f85bb739aa199e0e3d6094e875616df8747
John Enderby <john.enderby.1987@gmail.com>
2024-05-30 11:31:46 +0100

    nvim: updated neo-tree settings to show hidden files and dotfiles

--- .config/nvim/lua/core/plugin_config/neo-tree.lua
+++ .config/nvim/lua/core/plugin_config/neo-tree.lua
@@ -1,4 +1,19 @@
 require("neo-tree").setup({
+  filesystem = {
+    filtered_items = {
+      visible = true,
+      show_hidden_count = true,
+      hide_dotfiles = false,
+      hide_gitignored = false,
+      hide_by_name = {
+        ".git/",
+        ".DS_Store",
+        "thumbs.db",
+        "node_modules/",
+      },
+      never_show = {},
+    },
+  },
   popup_border_style = "single",
   sources = {
     "filesystem",
