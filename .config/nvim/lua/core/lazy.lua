1a909c405f129bb06a025dfd3c1caa02f470ffb9
John Enderby <john.enderby.1987@gmail.com>
2024-06-06 23:47:58 +0100

    nvim: added Comment plugin for react and typescriptreact

--- .config/nvim/lua/core/lazy.lua
+++ .config/nvim/lua/core/lazy.lua
@@ -29,31 +29,36 @@ local plugins = {
   -----------------------------------------------------------------------------
   "tpope/vim-sleuth", -- detect tab stops and shiftwidth automatically
   { "numToStr/Comment.nvim", lazy = false, event = "User FileOpened" }, -- faster commenting
-  { "folke/which-key.nvim", event = "VimEnter", cmd = "WhichKey", }, -- dhows current keymaps
+  { "JoosepAlviste/nvim-ts-context-commentstring", event = "User FileOpened" }, -- context aware commmenting for react etc.
+  { "folke/which-key.nvim", event = "VimEnter", cmd = "WhichKey" }, -- dhows current keymaps
   { "folke/twilight.nvim", event = "VeryLazy" }, -- dims unfocused code
   { "tris203/precognition.nvim", event = "User FileOpened" }, -- hints at motion keymaps
-  { "windwp/nvim-autopairs", config = true, },-- autopairs
-  { "tpope/vim-surround", event = { "BufReadPre", "BufNewFile" }, }, -- vim surround - mappings for surrounding text with delimeters
-  { "windwp/nvim-ts-autotag", ft = {"html", "jsx", "tsx" }, }, -- autotag, tag completion in html, js, jsx, etc...
-  { "stevearc/conform.nvim", lazy = false, },
+  { "windwp/nvim-autopairs", config = true }, -- autopairs
+  { "tpope/vim-surround", event = { "BufReadPre", "BufNewFile" } }, -- vim surround - mappings for surrounding text with delimeters
+  { "windwp/nvim-ts-autotag", ft = { "html", "jsx", "tsx" } }, -- autotag, tag completion in html, js, jsx, etc...
+  { "stevearc/conform.nvim", lazy = false },
   { "aznhe21/actions-preview.nvim", event = "User FileOpened" }, -- preview code actions
-  ----------------------------------------------------------------------------- 
+  -----------------------------------------------------------------------------
   -- UI
   -----------------------------------------------------------------------------
-  { "Bekaboo/deadcolumn.nvim", event = { "BufReadPre", "BufNewFile" }, },-- better color column support
+  { "Bekaboo/deadcolumn.nvim", event = { "BufReadPre", "BufNewFile" } }, -- better color column support
   { "lewis6991/gitsigns.nvim", event = "User FileOpened", cmd = "GitSigns" }, -- show git status in the gutter
   { "lukas-reineke/indent-blankline.nvim" }, -- show indent guides
   { "nvim-lua/popup.nvim", lazy = true }, -- An implementation of the Popup API from vim in NeoVim
   { "NvChad/nvim-colorizer.lua", event = "User FileOpened" }, -- Colorizer - colors hex, rgb, hsl colors in code
   { "folke/todo-comments.nvim", event = "VeryLazy" }, -- highlights todos
   { "folke/trouble.nvim", event = "User FileOpened" }, -- shows diagnostics, symbols and other useful things in a window
-  { "folke/noice.nvim", event = "VeryLazy",
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
     dependencies = {
       "MunifTanjim/nui.nvim",
-      { "rcarriga/nvim-notify", opts = { top_down = false, }, },
+      { "rcarriga/nvim-notify", opts = { top_down = false } },
     },
   }, -- ui plugin for "noice" messages, cmdline, and popup menu
-  { "nvim-neo-tree/neo-tree.nvim", branch = "v3.x",
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons",
@@ -63,15 +68,20 @@ local plugins = {
     cmd = { "Neotree show", "Neotree close", "Neotree toggle", "Neotree focus" },
   }, -- filetree explorer and buffers as tabs
   { "kosayoda/nvim-lightbulb", event = "User FileOpened" }, -- shows a lightbulb as virtual text when code actions are available
-  { "goolord/alpha-nvim", event = "VimEnter", }, -- customisable dashboard with two themes/layouts
+  { "goolord/alpha-nvim", event = "VimEnter" }, -- customisable dashboard with two themes/layouts
   { "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } }, -- customisable status linie
-  { "utilyre/barbecue.nvim", name = "barbecue", version = "*", event = { "BufReadPre", "BufNewFile" },
+  {
+    "utilyre/barbecue.nvim",
+    name = "barbecue",
+    version = "*",
+    event = { "BufReadPre", "BufNewFile" },
     dependencies = {
       "SmiteshP/nvim-navic",
       "nvim-tree/nvim-web-devicons",
     },
   }, -- breadcrumbs for symbols like VSCode
-  { "willothy/nvim-cokeline",
+  {
+    "willothy/nvim-cokeline",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons",
@@ -79,7 +89,7 @@ local plugins = {
     },
     config = true,
   }, -- custom tab styling
-  { "RRethy/vim-illuminate", event = "User FileOpened", }, -- highlight occurrences of word under cursor
+  { "RRethy/vim-illuminate", event = "User FileOpened" }, -- highlight occurrences of word under cursor
 
   ---------------------------------------------------------------------------
   -- Completion
@@ -88,30 +98,38 @@ local plugins = {
     "hrsh7th/nvim-cmp", -- completion engine
     event = { "InsertEnter", "CmdlineEnter" },
     dependencies = {
-      { "hrsh7th/cmp-nvim-lsp", },
-      { "hrsh7th/cmp-emoji", },
-      { "hrsh7th/cmp-buffer", },
-      { "hrsh7th/cmp-path", },
-      { "hrsh7th/cmp-cmdline", },
-      { "saadparwaiz1/cmp_luasnip", },
-      { "L3MON4D3/LuaSnip", build = "make install_jsregexp",
-        dependencies = {
-          "rafamadriz/friendly-snippets",
-        }, },
-      { "hrsh7th/cmp-nvim-lua", event = "InsertEnter", },
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "hrsh7th/cmp-emoji" },
+      { "hrsh7th/cmp-buffer" },
+      { "hrsh7th/cmp-path" },
+      { "hrsh7th/cmp-cmdline" },
+      { "saadparwaiz1/cmp_luasnip" },
+      { "L3MON4D3/LuaSnip", build = "make install_jsregexp", dependencies = {
+        "rafamadriz/friendly-snippets",
+      } },
+      { "hrsh7th/cmp-nvim-lua", event = "InsertEnter" },
+      { "mmolhoek/cmp-scss", event = "InsertEnter" },
     },
   },
 
   ---------------------------------------------------------------------------
   -- Telescope
   ---------------------------------------------------------------------------
-  { "nvim-telescope/telescope.nvim", event = "VimEnter", branch = "0.1.x", lazy = true, cmd = "Telescope",
+  {
+    "nvim-telescope/telescope.nvim",
+    event = "VimEnter",
+    branch = "0.1.x",
+    lazy = true,
+    cmd = "Telescope",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      { "nvim-telescope/telescope-fzf-native.nvim", build = "make",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
         cond = function()
           return vim.fn.executable "make" == 1
-        end, },
+        end,
+      },
       { "nvim-telescope/telescope-project.nvim" },
       { "nvim-telescope/telescope-ui-select.nvim" },
       { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
@@ -121,7 +139,11 @@ local plugins = {
   -------------------------------------------------------------------------
   -- LSP
   -------------------------------------------------------------------------
-  { "neovim/nvim-lspconfig", event = { "BufReadPre", "BufNewFile" }, cmd = { "LspInfo", "LspInstall", "LspUninstall" }, lazy = true,
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    cmd = { "LspInfo", "LspInstall", "LspUninstall" },
+    lazy = true,
     dependencies = {
       { "williamboman/mason.nvim", config = true, lazy = true, cmd = { "Mason", "MasonInstall", "MasonUninstall", "MasonUninstallAll", "MasonLog" } },
       { "williamboman/mason-lspconfig.nvim", lazy = true, event = "User FileOpened" }, -- helps with the above
@@ -130,7 +152,7 @@ local plugins = {
       { "nvimtools/none-ls.nvim" },
     },
   },
-  { "b0o/schemastore.nvim", lazy = true, },
+  { "b0o/schemastore.nvim", lazy = true },
 
   ---------------------------------------------------------------------------
   -- Treesitter
@@ -139,18 +161,18 @@ local plugins = {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     cmd = {
-    "TSInstall",
-    "TSUninstall",
-    "TSUpdate",
-    "TSUpdateSync",
-    "TSInstallInfo",
-    "TSInstallSync",
-    "TSInstallFromGrammar",
+      "TSInstall",
+      "TSUninstall",
+      "TSUpdate",
+      "TSUpdateSync",
+      "TSInstallInfo",
+      "TSInstallSync",
+      "TSInstallFromGrammar",
     },
     event = "User FileOpened",
   },
   { "HiPhish/rainbow-delimiters.nvim" },
-  
+
   ---------------------------------------------------------------------------
   -- Toggle term
   ---------------------------------------------------------------------------
