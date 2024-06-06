7f35cebd529895cffc73e106ceb99688bbe69eba
John Enderby <john.enderby.1987@gmail.com>
2024-06-06 23:48:12 +0100

    nvim: formatting for jsx and tsx

--- .config/nvim/lua/core/plugin_config/conform.lua
+++ .config/nvim/lua/core/plugin_config/conform.lua
@@ -15,6 +15,9 @@ require("conform").setup {
   formatters_by_ft = {
     lua = { "stylua" },
     javascript = { { "prettierd", "prettier" } },
+    javascriptreact = { { "prettierd", "prettier" } },
+    typescript = { { "prettierd", "prettier" } },
+    typescriptreact = { { "prettierd", "prettier" } },
     css = { { "prettierd", "prettier" } },
     scss = { { "prettierd", "prettier" } },
   },
