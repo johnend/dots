6a89fcd686fcbcc9a817ec785dd5b8ca877c17a2
John Enderby <john.enderby.1987@gmail.com>
2024-06-16 00:07:24 +0100

    nvim: undo plugin updates
    - removed undo tree and keybinds
    - added telescope undo - works the same, better UI, and more obvious
    customisation

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -78,16 +78,9 @@ keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
--- Toggle Undo tree
-keymap("n", "<leader>tu", ":UndotreeToggle<cr>", {desc = "Undotree"})
 -- Terminal --
 -- Better terminal navigation
 -- keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opt)
 -- keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 -- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 -- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
-
--- [[ Auto formatting ]]
-vim.keymap.set("n", "<leader>f", function()
-  require("conform").format { async = true, lsp_fallback = true }
-end, { desc = "Format buffer" })
