2a148ea08d2ab75c9b279dc28724e9129dbb31b1
John Enderby <john.enderby.1987@gmail.com>
2024-06-17 01:33:57 +0100

    nvim: autosave removed defer save
    - prefer only having autosave for focus lost
    - defer save is annoying with configs

--- .config/nvim/lua/core/plugins/helpers/autosave.lua
+++ .config/nvim/lua/core/plugins/helpers/autosave.lua
@@ -16,11 +16,15 @@ return {
           return (icons.misc.CircuitBoard .. " Saved: " .. vim.fn.expand "%:t")
         end,
         dim = 0.18,
-        cleaning_interval = 700,
+        cleaning_interval = 500,
       },
       trigger_events = {
         immediate_save = { "BufLeave", "FocusLost" },
+        -- uncomment the following lines to turn off saving after buffer changes
+        defer_save = {},
+        cancel_defered_save = {},
       },
+      debounce_delay = 1500,
     }
   end,
 }
