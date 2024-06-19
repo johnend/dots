5cad93fd32c387d1fb88ecab42ee8e4b89c8e2aa
John Enderby <john.enderby.1987@gmail.com>
2024-06-19 22:28:31 +0100

    nvim: removed autocommands in favour of illuminate plugin
    - flashing is never cool

--- .config/nvim/lua/core/plugins/lsp/configs/lsp.lua
+++ .config/nvim/lua/core/plugins/lsp/configs/lsp.lua
@@ -51,45 +51,6 @@ M.config = function()
       -- WARN: This is not Goto Definition, this is Goto Declaration.
       --  For example, in C this would take you to the header.
       map("gD", vim.lsp.buf.declaration, "Goto declaration")
-
-      -- The following two autocommands are used to highlight references of the
-      -- word under your cursor when your cursor rests there for a little while.
-      --    See `:help CursorHold` for information about when this is executed
-      --
-      -- When you move your cursor, the highlights will be cleared (the second autocommand).
-      local client = vim.lsp.get_client_by_id(event.data.client_id)
-      if client and client.server_capabilities.documentHighlightProvider then
-        local highlight_augroup = vim.api.nvim_create_augroup("quantum-lsp-highlight", { clear = false })
-        vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-          buffer = event.buf,
-          group = highlight_augroup,
-          callback = vim.lsp.buf.document_highlight,
-        })
-
-        vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-          buffer = event.buf,
-          group = highlight_augroup,
-          callback = vim.lsp.buf.clear_references,
-        })
-
-        vim.api.nvim_create_autocmd("LspDetach", {
-          group = vim.api.nvim_create_augroup("quantum-lsp-detach", { clear = true }),
-          callback = function(event2)
-            vim.lsp.buf.clear_references()
-            vim.api.nvim_clear_autocmds { group = "quantum-lsp-highlight", buffer = event2.buf }
-          end,
-        })
-      end
-
-      -- The following autocommand is used to enable inlay hints in your
-      -- code, if the language server you are using supports them
-      --
-      -- This may be unwanted, since they displace some of your code
-      if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
-        map("<leader>th", function()
-          vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-        end, "Inlay hints")
-      end
     end,
   })
 
