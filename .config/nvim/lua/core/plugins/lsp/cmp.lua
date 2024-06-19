133af9821a0bedf764de35eb6565da2fc2cf066d
John Enderby <john.enderby.1987@gmail.com>
2024-06-19 10:49:07 +0100

    nvim: updated cmp settings

--- .config/nvim/lua/core/plugins/lsp/cmp.lua
+++ .config/nvim/lua/core/plugins/lsp/cmp.lua
@@ -113,14 +113,14 @@ return {
         ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
         ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
         ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-        ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+        ["<C-y>"] = cmp.mapping.confirm { select = true }, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
         ["<C-c>"] = cmp.mapping {
           i = cmp.mapping.abort(),
           c = cmp.mapping.close(),
         },
         -- Accept currently selected item. If none selected, `select` first item.
         -- Set `select` to `false` to only confirm explicitly selected items.
-        ["<CR>"] = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Replace, select = false },
+        ["<CR>"] = cmp.mapping.confirm { select = true },
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
@@ -165,10 +165,6 @@ return {
             folders = { "node_modules/@fanduel/formation-tokens/build/scss" },
           },
         },
-      },
-      confirm_opts = {
-        behavior = cmp.ConfirmBehavior.Replace,
-        select = false,
       },
       window = {
         completion = cmp.config.window.bordered(),
