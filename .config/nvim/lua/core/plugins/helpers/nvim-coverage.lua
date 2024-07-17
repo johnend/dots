87e32971f14b4ea16a89121583c2b57cbb7890fc
John Enderby <john.enderby.1987@gmail.com>
2024-07-17 22:28:11 +0100

    nvim: added nvim coverage for test coverage

---
+++ .config/nvim/lua/core/plugins/helpers/nvim-coverage.lua
@@ -0,0 +1,25 @@
+return {
+  "andythigpen/nvim-coverage",
+  dependencies = { "nvim-lua/plenary.nvim" },
+
+  config = function()
+    local status_ok, coverage = pcall(require, "coverage")
+    if not status_ok then
+      return
+    end
+    coverage.setup {
+      auto_reload = true,
+      commands = true,
+      highlights = {
+        covered = { fg = colors.crust, bg = colors.green },
+        uncovered = { fg = colors.crust, bg = colors.red },
+      },
+      signs = {
+        covered = { text = icons.ui.TestCovered },
+        uncovered = { text = icons.ui.TestUncovered },
+      },
+    }
+
+    vim.keymap.set("n", "<leader>tc", "<cmd>CoverageToggle<cr>", { desc = "Test Coverage" })
+  end,
+}
