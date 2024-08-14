f074ff92a9b9dcc70dffe954b919c24208026512
John Enderby <john.enderby.1987@gmail.com>
2024-08-14 10:13:38 +0100

    hyprland: initial config

---
+++ .config/hypr/swww/walls.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR=$HOME/Pictures/Wallpapers/anime/
+WALLS=($(ls "${DIR}"))
+
+RANDOMWALL=${WALLS[ $RANDOM % ${#WALLS[@]} ]}
+
+if [[ $(pidof swaybg) ]]; then
+  pkill swaybg
+fi
+
+swww query || swww init
+
+# Change to random wallpaper in the Pictures directory
+swww img ${DIR}/${RANDOMWALL} --transition-fps 60 --transition-type any --transition-duration 3
--- .zshrc
+++ .zshrc
@@ -128,3 +128,10 @@ export PATH=$PATH:/home/johne/.spicetify
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+# bun completions
+[ -s "/home/johne/.bun/_bun" ] && source "/home/johne/.bun/_bun"
+
+# bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
