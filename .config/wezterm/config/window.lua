ec607402b3756007529b0172026bc6f7ad1a5a7f
John Enderby <john.enderby.1987@gmail.com>
2024-09-19 16:22:54 +0100

    wezterm: temporarily disabled tab bar

--- .config/wezterm/config/window.lua
+++ .config/wezterm/config/window.lua
@@ -3,6 +3,8 @@ local wezterm = require "wezterm"
 local window = {}
 
 function window.apply_to_config(config)
+  -- toggle this to enable/disable tabs
+  config.enable_tab_bar = false
   -- tab bar
   config.use_fancy_tab_bar = false
   config.tab_bar_at_bottom = true
