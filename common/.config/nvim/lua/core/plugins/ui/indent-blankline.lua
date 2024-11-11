8dd299b89882eb9a51a887be1c1252f44c211422
John Enderby <john.enderby.1987@gmail.com>
2024-11-11 10:25:03 +0000

    nvim: updates and default indent-blankline to off

--- common/.config/nvim/lua/core/plugins/ui/indent-blankline.lua
+++ common/.config/nvim/lua/core/plugins/ui/indent-blankline.lua
@@ -18,6 +18,7 @@ return {
     }
 
     ibl.setup {
+      enabled = false,
       indent = { highlight = highlight, char = "" },
       whitespace = { highlight = highlight, remove_blankline_trail = false },
       scope = { enabled = false },
