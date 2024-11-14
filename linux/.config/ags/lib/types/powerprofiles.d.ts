4b8c069d879be03777c3a64b665f94fb0d67c672
John Enderby <john.enderby.1987@gmail.com>
2024-11-14 21:59:22 +0000

    ags: removed ags from dots
    - using hyprpanel so no required

--- linux/.config/ags/lib/types/powerprofiles.d.ts
+++
@@ -1,8 +0,0 @@
-import icons from 'modules/icons/index';
-import PowerProfiles from 'types/service/powerprofiles.js';
-
-export type PowerProfiles = InstanceType<typeof PowerProfiles>;
-export type PowerProfile = 'power-saver' | 'balanced' | 'performance';
-export type PowerProfileObject = {
-    [key: string]: string;
-};
