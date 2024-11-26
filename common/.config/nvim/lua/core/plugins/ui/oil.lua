fec555c23a7caf987beb8e56c37f814aeff04c37
John Enderby <john.enderby.1987@gmail.com>
2024-11-26 22:34:30 +0000

    nvim: updated oil keymaps to avoid nav conflicts

--- common/.config/nvim/lua/core/plugins/ui/oil.lua
+++ common/.config/nvim/lua/core/plugins/ui/oil.lua
@@ -12,6 +12,25 @@ return {
       view_options = {
         show_hidden = true,
       },
+      use_default_keymaps = false,
+      keymaps = {
+        ["g?"] = "actions.show_help",
+        ["<CR>"] = "actions.select",
+        ["<C-s>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
+        ["<C-v>"] = { "actions.select", opts = { horizontal = true }, desc = "Open the entry in a horizontal split" },
+        ["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = "actions.close",
+        ["<C-l>"] = "actions.refresh",
+        ["-"] = "actions.parent",
+        ["_"] = "actions.open_cwd",
+        ["`"] = "actions.cd",
+        ["~"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory", mode = "n" },
+        ["gs"] = "actions.change_sort",
+        ["gx"] = "actions.open_external",
+        ["g."] = "actions.toggle_hidden",
+        ["g\\"] = "actions.toggle_trash",
+      },
     }
 
     vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open parent directory" })
