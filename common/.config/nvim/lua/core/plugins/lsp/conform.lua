781ec61af53e48ff59582811767546dfec6c5765
John Enderby <john.enderby.1987@gmail.com>
2024-12-10 16:59:11 +0000

    nvim: add keybind and user command for toggling formatting in buffers

--- common/.config/nvim/lua/core/plugins/lsp/conform.lua
+++ common/.config/nvim/lua/core/plugins/lsp/conform.lua
@@ -16,7 +16,12 @@ return {
 
     conform.setup {
       notify_on_error = false,
-      format_on_save = { timeout_ms = 500, lsp_fallback = true },
+      format_on_save = function(bufnr)
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return { timeout_ms = 500, lsp_fallback = true }
+      end,
       formatters_by_ft = {
         lua = { "stylua" },
         javascript = { "prettierd", "prettier", stop_after_first = true },
@@ -29,5 +34,21 @@ return {
         graphql = { "prettierd", "prettier", stop_after_first = true },
       },
     }
+
+    vim.api.nvim_create_user_command("ToggleFormat", function()
+      vim.notify = require "notify"
+      if vim.b.disable_autoformat then
+        vim.b.disable_autoformat = false
+        vim.notify "Auto format on save enabled for this buffer"
+      else
+        vim.b.disable_autoformat = true
+        ---@diagnostic disable-next-line: param-type-mismatch
+        vim.notify("Auto format on save disabled for this buffer", "warn")
+      end
+    end, {
+      desc = "Toggle auto format on save - buffer",
+    })
+
+    vim.keymap.set("n", "<leader>tf", ":ToggleFormat<cr>", { desc = "Toggle format on save (buffer)" })
   end,
 }
