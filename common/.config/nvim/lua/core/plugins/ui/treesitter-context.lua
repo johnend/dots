98abaabed925f8f76410ad2f702f5a92be254f12
John Enderby <john.enderby.1987@gmail.com>
2024-12-11 00:04:27 +0000

    nvim: fix treesitter context so that it actually works

--- common/.config/nvim/lua/core/plugins/ui/treesitter-context.lua
+++ common/.config/nvim/lua/core/plugins/ui/treesitter-context.lua
@@ -1,10 +1,24 @@
 return {
   "nvim-treesitter/nvim-treesitter-context",
+  event = "VeryLazy",
   config = function()
     local status_ok, tscontext = pcall(require, "treesitter-context")
     if not status_ok then
       return
     end
-    tscontext.setup = {}
+
+    tscontext.setup {
+      enable = true,
+      multiwindow = false,
+      max_lines = 0,
+      min_window_height = 0,
+      line_numbers = true,
+      multiline_threshold = 20,
+      trim_scope = "outer",
+      mode = "cursor",
+      separator = nil,
+      zindex = 20,
+      on_attach = nil,
+    }
   end,
 }
