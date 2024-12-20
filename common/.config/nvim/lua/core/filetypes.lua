68c81b62f0f592b888a6f0ff416879aa25ba9498
John Enderby <john.enderby.1987@gmail.com>
2024-12-20 19:55:35 +0000

    nvim: fie type icon for cfg files

--- common/.config/nvim/lua/core/filetypes.lua
+++ common/.config/nvim/lua/core/filetypes.lua
@@ -10,11 +10,21 @@ vim.filetype.add {
     [".env"] = "dotenv",
     ["tsconfig.json"] = "jsonc",
     [".yamlfmt"] = "yaml",
+    [".cfg"] = "cfg",
   },
   pattern = {
     ["%.env%.[%w_.-]+"] = "bash",
     [".*/kitty/.*%.conf"] = "bash",
     [".*%.rc"] = "bash",
     [".*/hypr/.*%.conf"] = "hyprlang",
+  },
+}
+
+-- custom filetype icons
+
+require("nvim-web-devicons").set_icon {
+  ["cfg"] = {
+    icon = "",
+    name = "Configuration",
   },
 }
