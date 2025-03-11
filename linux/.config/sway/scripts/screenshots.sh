af10102437f7d15ad1467187d7ed55125d8c3850
John Enderby <john.enderby.1987@gmail.com>
2025-03-11 23:03:37 +0000

    linux: sway screenshot script

---
+++ linux/.config/sway/scripts/screenshots.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Determine screenshot type based on argument
+case "$1" in
+  fullscreen)
+    file="$HOME/Pictures/Screenshots/fullscreen-$(date +'%d-%m-%Y_%H-%M-%S').png"
+    grim "$file"  # Fullscreen screenshot
+    ;;
+  window)
+    file="$HOME/Pictures/Screenshots/window-$(date +'%d-%m-%Y_%H-%M-%S').png"
+    swaymsg -t get_tree | jq -r '.. | (.nodes? // empty)[] | select(.pid and .visible) | .rect | "\(.x),\(.y) \(.width)x\(.height)"' | \
+    slurp | grim -g - "$file"  # Window screenshot
+    ;;
+  area)
+    file="$HOME/Pictures/Screenshots/screenshot-$(date +'%d-%m-%Y_%H-%M-%S').png"
+    grim -g "$(slurp)" "$file"  # Area screenshot
+    ;;
+  *)
+    notify-send "Error" "Unknown screenshot type"  # If invalid argument is passed
+    exit 1
+    ;;
+esac
+
+# Show notification with options
+notify-send "Screenshot Taken" "$file" -i "$file"
+  # --action="delete=delete" \
+  # --action="open=open"
+
+# # Store the filename in /tmp for reference
+# echo "$file" > /tmp/last_screenshot
+
+# action_index=$(swaync-client -a)
+# echo "Action index output: $action_index" >> ~/swaync_debug.log
+# case "$action_index" in
+#   "0")
+#     action="open"
+#     ;;
+#   "1")
+#     action="delete"
+#     ;;
+#   "*")
+#     action="unknown"
+#     ;;
+# esac
+# echo "Action being processed: $action"
+#
+# case "$action" in
+#   "open")
+#     nemo "$file"
+#     ;;
+#   "delete")
+#     rm "$file" && notify-send "Screenshot deleted" "$file"
+#     ;;
+#   *)
+#     notify-send "Action error" "Unknown action: $action"
+#     ;;
+# esac
