a66bb30df1279960bf750b91357742aa9e6104a7
John Enderby <john.enderby.1987@gmail.com>
2025-04-16 17:54:47 +0100

    commmon: nvim - add nvim-dap and crates
    - crates for easily managing cargo crates

---
+++ common/.config/nvim/lua/core/plugins/devtools/crates.lua
@@ -0,0 +1,27 @@
+local completion = require "vim.lsp.completion"
+return {
+  "saecki/crates.nvim",
+  tag = "stable",
+  event = { "BufRead Cargo.toml" },
+
+  config = function()
+    local status_ok, crates = pcall(require, "crates")
+    if not status_ok then
+      return
+    end
+
+    crates.setup {
+      completion = {
+        crates = { enabled = true, max_results = 8, min_chars = 3 },
+        cmp = { enabled = true },
+      },
+      popup = {
+        border = "rounded",
+      },
+      null_ls = {
+        enabled = true,
+        name = "crates.nvim",
+      },
+    }
+  end,
+}
---
+++ common/.config/nvim/lua/core/plugins/devtools/nvim-dap.lua
@@ -0,0 +1,11 @@
+return {
+  "mfussenegger/nvim-dap",
+  event = { "VeryLazy" },
+
+  config = function()
+    local status_ok, dap = pcall(require, "nvim-dap")
+    if not status_ok then
+      return
+    end
+  end,
+}
