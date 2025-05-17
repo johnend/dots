747672c8dbcf0acbd68ba9bf6aa84c42b644875c
John Enderby <john.enderby.1987@gmail.com>
2025-05-18 00:36:16 +0100

    common: nvim - lightbulb updates
    - might remove this as it feels noisy

--- common/.config/nvim/lua/core/plugins/ui/lightbulb.lua
+++ common/.config/nvim/lua/core/plugins/ui/lightbulb.lua
@@ -2,16 +2,30 @@ return {
   "kosayoda/nvim-lightbulb",
   event = "BufRead",
   config = function()
-    require("nvim-lightbulb").update_lightbulb {
+    local status_ok, lightbulb = pcall(require, "nvim-lightbulb")
+    if not status_ok then
+      return
+    end
+
+    lightbulb.setup {
+      autocmd = { enabled = true },
       sign = {
         enabled = false,
       },
       virtual_text = {
         enabled = true,
+        text = icons.ui.Lightbulb,
       },
       status_text = {
         enabled = true,
       },
+      filter = function(client_name, action)
+        local title = (action.title or ""):lower()
+        if title == "move to a new file" then
+          return false
+        end
+        return true
+      end,
     }
   end,
 }
