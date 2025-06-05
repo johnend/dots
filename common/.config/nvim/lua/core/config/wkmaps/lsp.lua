34cd25188ef145a93aa84fa05414536420b3bdf1
John Enderby <john.enderby.1987@gmail.com>
2025-06-06 00:51:49 +0100

    common: nvim - trouble keymap updates

--- common/.config/nvim/lua/core/config/wkmaps/lsp.lua
+++ common/.config/nvim/lua/core/config/wkmaps/lsp.lua
@@ -9,16 +9,22 @@ return {
     desc = "Check TypeScript types",
   },
   { "<leader>li", ":LspInfo<cr>", desc = "LSP info" },
-  { "<leader>ll", ":Trouble loclist toggle<cr>", desc = "Location list (Trouble)" },
-  { "<leader>lq", ":Trouble qflist toggle<cr>", desc = "Quickfix list (Trouble)" },
-  { "<leader>lv", ":Trouble lsp toggle focus=false<cr>", desc = "LSP toggle (Trouble)" },
-  { "<leader>lR", ":LspRestart<cr>", desc = "Restart LSP Server" },
+  { "<leader>ll", ":Trouble loclist toggle<cr>", desc = "Location list" },
+  { "<leader>lq", ":Trouble qflist toggle<cr>", desc = "Quickfix list" },
+  { "<leader>lv", ":Trouble lsp toggle focus=false<cr>", desc = "LSP toggle" },
+  -- TODO: need to create a picker that passes LSP name to LspRestart
+  { "<leader>lr", ":LspRestart<cr>", desc = "Restart LSP Server" },
+  {
+    "<leader>ls",
+    ":Trouble symbols<cr>",
+    desc = "Document Symbols",
+  },
   -------------------------------------
   --- Diagnostics
   -------------------------------------
   { "<leader>ld", group = "Diagnostics" },
-  { "<leader>ldX", ":Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer diagnostics (Trouble)" },
-  { "<leader>ldx", ":Trouble diagnostics toggle<CR>", desc = "Diagnostics (Trouble)" },
+  { "<leader>ldX", ":Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer diagnostics" },
+  { "<leader>ldx", ":Trouble diagnostics toggle<CR>", desc = "Diagnostics" },
   -------------------------------------
   --- Quickfix
   -------------------------------------
