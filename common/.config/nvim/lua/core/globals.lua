8099b1ef0f95ca20f75771936bad8481e023265f
John Enderby <john.enderby.1987@gmail.com>
2025-07-14 10:05:44 +0100

    common: nvim - comment out cokeline global

--- common/.config/nvim/lua/core/globals.lua
+++ common/.config/nvim/lua/core/globals.lua
@@ -2,7 +2,7 @@
 _G._diagnostics_virtual_text_enabled = false
 
 -- Default cokeline state
-_G._cokeline_enabled = false
+-- _G._cokeline_enabled = false
 
 -- Inlay hints state
 _G._inlay_hints_enabled = false
