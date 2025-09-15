b7b45bdaba5d505f03d32874b76287e695e8620d
John Enderby <john.enderby.1987@gmail.com>
2025-09-15 17:15:22 +0100

    common: nvim - command for disable/enable format on save

--- common/.config/nvim/lua/core/plugins/lsp/conform.lua
+++ common/.config/nvim/lua/core/plugins/lsp/conform.lua
@@ -35,4 +35,23 @@ return {
       },
     }
   end,
+
+  -- Register user command to enable/disable formatting
+  vim.api.nvim_create_user_command("FormatDisable", function(args)
+    if args.bang then
+      -- FormatDisable! will disable formatting just for this buffer
+      vim.b.disable_autoformat = true
+    else
+      vim.g.disable_autoformat = true
+    end
+  end, {
+    desc = "Disable autoformat-on-save",
+    bang = true,
+  }),
+  vim.api.nvim_create_user_command("FormatEnable", function()
+    vim.b.disable_autoformat = false
+    vim.g.disable_autoformat = false
+  end, {
+    desc = "Re-enable autoformat-on-save",
+  }),
 }
