bcbef3c893c82e53e733be68e728f3adbc9735cb
John Enderby <john.enderby.1987@gmail.com>
2025-09-24 22:28:43 +0100

    common: nvim - add WIP to todo comments

--- common/.config/nvim/lua/core/plugins/editing/todo-comments.lua
+++ common/.config/nvim/lua/core/plugins/editing/todo-comments.lua
@@ -20,6 +20,7 @@ return {
         PERF = { icon = "󰓅 ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
         NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
         TEST = { icon = "󰙨 ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+        WIP = { icon = "󱌢 ", color = "info" },
       },
       highlight = {
         multiline = false,
