911a10f7bd698de03104fcdacc9d9167a4956df3
John Enderby <john.enderby.1987@gmail.com>
2026-02-15 22:52:55 +0000

    common: nvim - highlight overrides
    - setup function for globally overriding colorscheme highlights
    - this is so that I can make them behave consistently across
    colorschemes
    - this WILL result in some colorschemes not looking the same as their
    default implementations

--- common/.config/nvim/lua/core/highlights.lua
+++ common/.config/nvim/lua/core/highlights.lua
@@ -3,8 +3,8 @@
 
 local M = {}
 
--- Remove underlines from which-key icons
--- This runs after which-key's ColorScheme autocmd to fix icon highlights
+---Remove underlines from which-key icons
+---This runs after which-key's ColorScheme autocmd to fix icon highlights
 function M.setup_whichkey_fixes()
   vim.api.nvim_create_autocmd("ColorScheme", {
     group = vim.api.nvim_create_augroup("FixWhichKeyIconUnderlines", { clear = true }),
@@ -38,8 +38,28 @@ function M.setup_whichkey_fixes()
   })
 end
 
+---Fix highlights across colorschemes in a consistent way by linking to existing highlight groups
+---Should be careful working with these, as they're global
+---Colorschemes that apply highlights different for visual style should be considered
+function M.fix_highlights()
+  vim.api.nvim_create_autocmd("VimEnter", {
+    group = vim.api.nvim_create_augroup("FixHighlights", { clear = true }),
+    callback = function()
+      ---Link an existing highlight (from pluging etc.) to a pre-existing highlight group
+      ---@param initialName string
+      ---@param linkName string
+      local override = function(initialName, linkName)
+        vim.api.nvim_set_hl(0, initialName, { link = linkName, default = true })
+      end
+
+      override("TreesitterContextSeparator", "Comment")
+    end,
+  })
+end
+
 function M.setup()
   M.setup_whichkey_fixes()
+  M.fix_highlights()
   -- Add more custom highlight fixes here in the future
 end
 
